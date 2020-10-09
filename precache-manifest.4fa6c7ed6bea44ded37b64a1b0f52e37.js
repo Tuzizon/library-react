77fcfea76e5c3c520a18dd3a9c7ec31692f7511e
Arthur Bastos <tuzaobastos@gmail.com>
2020-10-09 00:31:22 -0300

    Updates

---
+++ precache-manifest.4fa6c7ed6bea44ded37b64a1b0f52e37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e706ae46709a5eec228e0055692e06c",
+    "url": "/library-react/index.html"
+  },
+  {
+    "revision": "18b0bdb5c90ef8b6d8b6",
+    "url": "/library-react/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "84252d4c00c0e1e48012",
+    "url": "/library-react/static/css/main.aa9bcb3b.chunk.css"
+  },
+  {
+    "revision": "18b0bdb5c90ef8b6d8b6",
+    "url": "/library-react/static/js/2.4bf2a587.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/library-react/static/js/2.4bf2a587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84252d4c00c0e1e48012",
+    "url": "/library-react/static/js/main.6956152b.chunk.js"
+  },
+  {
+    "revision": "f8eb2b255561862f59cf",
+    "url": "/library-react/static/js/runtime-main.28290705.js"
+  },
+  {
+    "revision": "6534237057db6656be1c06eae7cf85c0",
+    "url": "/library-react/static/media/logo.65342370.svg"
+  }
+]);
